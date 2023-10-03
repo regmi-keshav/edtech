a37092e408f815a8138d433ca3748d6c3c8be749
keshav_regmi <keshavregmi70@gmail.com>
2023-10-04 02:30:00 +0530

    Version 1

---
+++ src/components/about.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+export const About = (props) => {
+  return (
+    <div id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12 col-md-6">
+            {" "}
+            <img src="img/ab.jpg" width="600px" height="300px" alt="" />{" "}
+          </div>
+          <div className="col-xs-12 col-md-6">
+            <div className="about-text">
+              <h2>Features</h2>
+              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <h3>Why Choose Us?</h3>
+              <div className="list-style">
+                <div className="col-lg-10 col-sm-10 col-xs-12">
+                  <ul>
+                    {props.data
+                      ? props.data.Why.map((d, i) => (
+                          <li key={`${d}-${i}`}>{d}</li>
+                        ))
+                      : "loading"}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
