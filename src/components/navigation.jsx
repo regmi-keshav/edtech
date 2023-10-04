6252708eb26cf0acb981db824e438d77f715ceea
keshav_regmi <keshavregmi70@gmail.com>
2023-10-04 16:21:14 +0530

    update

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -18,7 +18,7 @@ export const Navigation = (props) => {
             <span className="icon-bar"></span>{" "}
           </button>
           <div>
-            <img src="/img/logo.jpg" height="60px" width="70px" alt="" />{" "}
+            <img src="/img/logo.jpg" height="60px" width="70px" alt="logo" />{" "}
             <a className="navbar-brand page-scroll" href="#page-top">
               VITTESHO
             </a>{" "}
